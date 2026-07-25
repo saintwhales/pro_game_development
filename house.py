25d15a980d262663da4ae43c71ca216fb7628aac
saintwhales <60368143+saintwhales@users.noreply.github.com>
2026-07-25 10:16:20 +0100

    Add files via upload

--- house.py
+++ house.py
@@ -13,7 +13,10 @@ while running:
 
     screen.fill((3, 219, 252))
 
-    pygame.draw.rect(screen, (207, 75, 19), (100, 75, 900, 400))
+    pygame.draw.rect(screen, (46, 230, 30), (0, 450, 1000, 250))
+    pygame.draw.rect(screen, (207, 75, 19), (300, 200, 400, 350))
+    pygame.draw.polygon(screen, (0, 0, 0), [(275, 200), (725, 200), (500, 50)])
+    pygame.draw.rect(screen, (156, 91, 22), (450, 400, 100, 150))
     pygame.display.update()
 
 pygame.quit()
